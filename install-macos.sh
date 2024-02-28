32b5677f072138e65bfc1e69fbdd0f4ff0a12978
Julien Da Cunha <jdacunha@alpian.com>
2024-02-28 11:19:19 +0100

    improve the sequence

--- install-macos.sh
+++ install-macos.sh
@@ -3,12 +3,14 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 xcode-select --install
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install tree git terraform tflint wget mongosh kubecolor helm htop mtr jq kubecolor k9s gcloud powerlevel10k
-
-echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
-echo $'autoload -Uz compinit\ncompinit\nget zsh complete kubectl\nsource <(kubectl completion zsh)\nalias kubectl=kubecolor\nmake completion work with kubecolor\ncompdef kubecolor=kubectl' >> ~/.zshrc
-
+brew install tree git terraform tflint wget mongosh kubecolor helm htop mtr jq kubecolor k9s gcloud 
 brew install --cask gitkraken arc shottr orbstack dbeaver-community vlc visual-studio-code sublime-text warp remote-desktop-manager drawio
 
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install powerlevel10k
+echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+
+echo $'autoload -Uz compinit\ncompinit\nget zsh complete kubectl\nsource <(kubectl completion zsh)\nalias kubectl=kubecolor\nmake completion work with kubecolor\ncompdef kubecolor=kubectl' >> ~/.zshrc
