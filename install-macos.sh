3c8e6781dc53358790964b52d2a3b32f5b7b4c1c
Julien Da Cunha <jdacunha@alpian.com>
2024-03-12 09:06:43 +0100

    add tfswitch

--- install-macos.sh
+++ install-macos.sh
@@ -4,7 +4,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 xcode-select --install
 
-brew install tree git terraform tflint wget mongosh helm htop mtr jq hidetatz/tap/kubecolor k9s gcloud 
+brew install tree git terraform tflint wget mongosh helm htop mtr jq hidetatz/tap/kubecolor k9s gcloud warrensbox/tap/tfswitch
 brew install --cask gitkraken arc shottr orbstack dbeaver-community vlc visual-studio-code sublime-text warp remote-desktop-manager drawio
 
 
@@ -14,3 +14,5 @@ brew install powerlevel10k
 echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 echo $'autoload -Uz compinit\ncompinit\nget zsh complete kubectl\nsource <(kubectl completion zsh)\nalias kubectl=kubecolor\nmake completion work with kubecolor\ncompdef kubecolor=kubectl' >> ~/.zshrc
+
+PATH=~/Users/juliendacunha/bin:$PATH
